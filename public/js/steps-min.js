a461ced18e0f8b6a085a4210f907119a949dd4ec
Camilo Niño <carastaman1@gmail.com>
2020-12-22 14:27:19 -0500

    add file
    se suben los files

---
+++ public/js/steps-min.js
@@ -0,0 +1,1 @@
+var tour=new Tour({steps:[{element:"#step1",title:"Title of my step",content:"Content of my step"},{element:"#step2",title:"Title of my step",content:"Content of my step"}]});tour.init(),tour.start();
